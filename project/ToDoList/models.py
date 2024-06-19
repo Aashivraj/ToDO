48ad75986a50ee5e9e1e6b4e7243b48ca51a862f
Aashivraj <saashivraj@gmail.com>
2024-06-19 15:13:20 +0530

    change

--- project/ToDoList/models.py
+++ project/ToDoList/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
 
 class Team(models.Model):
     team_department = models.CharField(max_length=100)
