f970e3707c9ddda2e09bd486c28a267d1af569ce
Devansh1312 <Shahdevansh13122002@gmail.com>
2024-06-24 18:54:05 +0530

    filter page

--- project/ToDoList/filters.py
+++ project/ToDoList/filters.py
@@ -1,9 +1,7 @@
 import django_filters
-import django_filters.widgets
 from .models import Todo
 from django import forms
 from django_filters import DateFilter
-
 
 class TodoFilter(django_filters.FilterSet):
     date_created = django_filters.DateFilter(
@@ -11,13 +9,12 @@ class TodoFilter(django_filters.FilterSet):
         widget=forms.DateInput(
             attrs={
                 'id': 'datepicker',
-                'type': 'text'
+                'type': 'text',
+                'class': 'datepicker theme-sensitive'  # Add a class to target the date picker
             }
         )
     )
-   
+
     class Meta:
         model = Todo
         fields = ['user', 'date_created', 'team']
-        
-        
